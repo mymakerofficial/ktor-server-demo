10959be8cdfb6b8a6726691a0e32bfac339099ed
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-11-10 15:11:53 +0100

    woop

--- src/main/kotlin/de/maiker/business/PreviewGenerator.kt
+++ src/main/kotlin/de/maiker/business/PreviewGenerator.kt
@@ -6,18 +6,20 @@ interface PreviewGeneratorSpec {
     fun generate(bytes : ByteArray, width : Int, height : Int) : ByteArray
 }
 
-class ImagePreviewGenerator : PreviewGeneratorSpec {
-    val imageScaler = ImageScaler()
+class ImagePreviewGenerator(
+    contentType: ContentType
+) : PreviewGeneratorSpec {
+    private val scaler = ImageScalerFactory.createImageScaler(contentType)
 
     override fun generate(bytes: ByteArray, width: Int, height: Int)
-        = imageScaler.scale(bytes, width, height)
+        = scaler.scale(bytes, width, height)
 }
 
 class PreviewGeneratorFactory {
     companion object {
         fun createPreviewGenerator(contentType: ContentType) : PreviewGeneratorSpec {
             if (contentType == ContentType.Image.Any)
-                return ImagePreviewGenerator()
+                return ImagePreviewGenerator(contentType)
 
             throw Exception("Unsupported content type")
         }
